5ff24ca327e17539da65c137ece0b6e7aae2442d
Alexander Thofern <Alexander.Thofern@gmx.de>
2024-04-25 01:15:37 +0200

    JSL1 alert functions

--- JSL1+2/js/jsl.js
+++ JSL1+2/js/jsl.js
@@ -1,336 +1,18 @@
 
-
 /**
- *  Function to change active view by switching between the classes list-view and tile-view
- *  changes button icons depending on activ view,
- *  triggers fading effect by using the button
+ * Modifiziert und angepasst durch Alexander Thofern
+ * Aufgaben JSL 1+2
  */
-/*
-function handleSwitchButton(){
-  const viewSwitch = document.getElementById("view-switch");
-  const iconSwitch = document.getElementById("switchButton");
-  const fadeInOut = document.getElementById("fadeInOutSwitch");
-  
-  
-  if (viewSwitch.classList.contains("main-list")) {
-
-    fadeInOut.classList.add("view-shown");
-    viewSwitch.classList.remove("main-list");
-    viewSwitch.classList.add("main-tile");
-    iconSwitch.classList.remove("icon-tiles");
-    iconSwitch.classList.add("icon-list");  
-  }
-  else{
-    viewSwitch.classList.remove("main-tile");
-    viewSwitch.classList.add("main-list");
-    iconSwitch.classList.remove("icon-list");
-    iconSwitch.classList.add("icon-tiles");
-
-  }
-};
-*/
-
-/**
- *  Function to change active view by switching between the classes list-view and tile-view
- *  changes button icons depending on activ view,
- *  triggers fading effect by using the button
- */
-/*
-function handleSwitchButton(){
-  const viewSwitch = document.getElementById("view-switch");
-  const iconSwitch = document.getElementById("switchButton");
-  const fadeInOut = document.getElementById("fadeInOutSwitch");
-  
-  
-  fadeInOut.classList.add("view-fadeout");
-  
-  setTimeout(() => {}, "2020"); 
-    if (viewSwitch.classList.contains("main-list")) {
-      viewSwitch.classList.remove("main-list");
-      viewSwitch.classList.add("main-tile");
-      iconSwitch.classList.remove("icon-tiles");
-      iconSwitch.classList.add("icon-list");  
-      fadeInOut.classList.add("view-fadein"); 
-         
-    }
-    fadeInOut.classList.remove("view-fadeout");
-
-
-};
-*/
-
-
-
-
-
 
 
 
 /**
- *  Function to change active view by switching between the classes list-view and tile-view
+ * JSL 1 
+ * Function to change active view by switching between the classes list-view and tile-view
  *  changes button icons depending on activ view,
- *  triggers fading effect by using the button
- */
-/*
-function handleSwitchButton(){
-  const viewSwitch = document.getElementById("view-switch");
-  const iconSwitch = document.getElementById("switchButton");
-   fadeout();
-  function fadeout(){
-
-    let interval=0;
-    interval=setInterval(hide, 50);
-    hide();
-  
-    function hide(){
-      const fadeInOut = document.getElementById("fadeInOutSwitch");
-      opacity = Number(window.getComputedStyle(fadeInOut).getPropertyValue("opacity"));
-      console.log(opacity);
-      if(opacity>0){    
-        opacity = opacity -0.1;
-        fadeInOut.style.opacity = opacity;
-        console.log(opacity);
-      }else{
-        clearInterval(interval);
-        if (viewSwitch.classList.contains("main-list")) {
-          viewSwitch.classList.remove("main-list");
-          viewSwitch.classList.add("main-tile");
-          iconSwitch.classList.remove("icon-tiles");
-          iconSwitch.classList.add("icon-list");
-        }
-        else{
-          viewSwitch.classList.remove("main-tile");
-          viewSwitch.classList.add("main-list");
-          iconSwitch.classList.remove("icon-list");
-          iconSwitch.classList.add("icon-tiles");
-        }
-      
-      } 
-      fadeIn();
-      function fadeIn(){
-
-        let interval=0;
-        interval=setInterval(hide, 50);
-        show();
-      
-        function show(){
-          const fadeInOut = document.getElementById("fadeInOutSwitch");
-          opacity = Number(window.getComputedStyle(fadeInOut).getPropertyValue("opacity"));
-          console.log(opacity);
-          if(opacity<0){    
-            opacity = opacity +0.1;
-            fadeInOut.style.opacity = opacity;
-            console.log(opacity);
-          }else{
-            clearInterval(interval);
-            if (viewSwitch.classList.contains("main-list")) {
-              viewSwitch.classList.remove("main-list");
-              viewSwitch.classList.add("main-tile");
-              iconSwitch.classList.remove("icon-tiles");
-              iconSwitch.classList.add("icon-list"); 
-    
-            if (viewSwitch.classList.contains("main-tile")) {
-              viewSwitch.classList.remove("main-tile");
-              viewSwitch.classList.add("main-list");
-              iconSwitch.classList.remove("icon-list");
-              iconSwitch.classList.add("icon-tiles");
-          } 
-          }
-  } 
-      }
-    }
-};
-}
-    }
-
-    */
-
-
-
-
-
-
-    /**
- *  Function to change active view by switching between the classes list-view and tile-view
- *  changes button icons depending on activ view,
- *  triggers fading effect by using the button
+ *  triggers fading out and in effect by using the button
  */
 
-// function handleSwitchButton(){
-//   const viewSwitch = document.getElementById("view-switch");
-//   const iconSwitch = document.getElementById("switchButton");
-//    fadeout();
-//   function fadeout(){
-
-//     let interval=0;
-//     interval=setInterval(hide, 50);
-//     hide();
-  
-//     function hide(){
-//       const fadeInOut = document.getElementById("fadeInOutSwitch");
-//       opacity = Number(window.getComputedStyle(fadeInOut).getPropertyValue("opacity"));
-//       console.log(opacity);
-//       if(opacity>0){    
-//         opacity = opacity -0.1;
-//         fadeInOut.style.opacity = opacity;
-//         console.log(opacity);
-//       }else{
-//         clearInterval(interval);
-//         if (viewSwitch.classList.contains("main-list")) {
-//           viewSwitch.classList.remove("main-list");
-//           viewSwitch.classList.add("main-tile");
-//           iconSwitch.classList.remove("icon-tiles");
-//           iconSwitch.classList.add("icon-list");
-//         }
-//         else{
-//           viewSwitch.classList.remove("main-tile");
-//           viewSwitch.classList.add("main-list");
-//           iconSwitch.classList.remove("icon-list");
-//           iconSwitch.classList.add("icon-tiles");
-//         }
-      
-//       } 
-//       fadeIn();
-//       function fadeIn(){
-
-//         let interval=0;
-//         interval=setInterval(hide, 50);
-//         show();
-      
-//         function show(){
-//           const fadeInOut = document.getElementById("fadeInOutSwitch");
-//           opacity = Number(window.getComputedStyle(fadeInOut).getPropertyValue("opacity"));
-//           console.log(opacity);
-//           if(opacity<0){    
-//             opacity = opacity +0.1;
-//             fadeInOut.style.opacity = opacity;
-//             console.log(opacity);
-//           }else{
-//             clearInterval(interval);
-//             if (viewSwitch.classList.contains("main-list")) {
-//               viewSwitch.classList.remove("main-list");
-//               viewSwitch.classList.add("main-tile");
-//               iconSwitch.classList.remove("icon-tiles");
-//               iconSwitch.classList.add("icon-list"); 
-    
-//             if (viewSwitch.classList.contains("main-tile")) {
-//               viewSwitch.classList.remove("main-tile");
-//               viewSwitch.classList.add("main-list");
-//               iconSwitch.classList.remove("icon-list");
-//               iconSwitch.classList.add("icon-tiles");
-//           } 
-//           }
-//   } 
-//       }
-//     }
-// };
-// }
-//     }
-
-function show() {
-    const fadeInOut = document.getElementById("fadeInOutSwitch");
-    opacity = Number(window.getComputedStyle(fadeInOut).getPropertyValue("opacity"));
-    if(opacity<0){    
-      opacity = opacity +0.1;
-      fadeInOut.style.opacity = opacity;
-    } else {
-      clearInterval(interval);
-      if (viewSwitch.classList.contains("main-list")) {
-        viewSwitch.classList.remove("main-list");
-        viewSwitch.classList.add("main-tile");
-        iconSwitch.classList.remove("icon-tiles");
-        iconSwitch.classList.add("icon-list"); 
-      }
-      if (viewSwitch.classList.contains("main-tile")) {
-        viewSwitch.classList.remove("main-tile");
-        viewSwitch.classList.add("main-list");
-        iconSwitch.classList.remove("icon-list");
-        iconSwitch.classList.add("icon-tiles");
-    } 
-  }
-}
-
-function fadeIn() {
-  let interval=0;
-  interval=setInterval(hide, 50);
-  show();
-} 
-
-function fadeout(){
-  let interval=0;
-  interval=setInterval(hide, 50);
-  hide();
-
-}
-
-function hide(){
-  const fadeInOut = document.getElementById("fadeInOutSwitch");
-  opacity = Number(window.getComputedStyle(fadeInOut).getPropertyValue("opacity"));
-  console.log(opacity);
-  if(opacity>0){    
-    opacity = opacity -0.1;
-    fadeInOut.style.opacity = opacity;
-    console.log(opacity);
-  }else{
-    clearInterval(interval);
-    if (viewSwitch.classList.contains("main-list")) {
-      viewSwitch.classList.remove("main-list");
-      viewSwitch.classList.add("main-tile");
-      iconSwitch.classList.remove("icon-tiles");
-      iconSwitch.classList.add("icon-list");
-    }
-    else{
-      viewSwitch.classList.remove("main-tile");
-      viewSwitch.classList.add("main-list");
-      iconSwitch.classList.remove("icon-list");
-      iconSwitch.classList.add("icon-tiles");
-    }
-  
-  } 
-};
-    
-function handleSwitchButton() {
-  const viewSwitch = document.getElementById("view-switch");
-  const iconSwitch = document.getElementById("switchButton");
-}
-
-
-{/* <div id="view-switch" class="main-list">
-      <div class="item"></div>
-      <div class="item"></div>
-      <div class="item"></div>
-      <div class="item"></div>
-      <div class="item"></div>
-    </div> */}
-
-{/* <button id="switchButton"> Change Me</button> */}
-
-// 1. Wir brauchen eine Funktion, die die View von List zu Tile, Tile zu List ändert
-function handleSwitchView1() {
-  const viewSwitch = document.getElementById("view-switch");
-  const iconSwitch = document.getElementById("switchButton");
-
-  const isTileView = viewSwitch.classList.contains("main-list") // gibt true oder false zurück
-
-  viewSwitch.classList.toggle("main-list", isTileView)
-  viewSwitch.classList.toggle("main-tile", !isTileView)
-  
-  iconSwitch.classList.toggle("icon-list", isTileView)
-  iconSwitch.classList.toggle("icon-tiles", !isTileView)
-}
-
-// 2. Wir brauchen eine Funktion, die beim wechsel der View einen Fade Out und Fade In erzeugt
-
-
-{/* <div id="view-switch" class="main-list">
-      <div class="item"></div>
-      <div class="item"></div>
-      <div class="item"></div>
-      <div class="item"></div>
-      <div class="item"></div>
-    </div> */}
-
-    
 function handleSwitchView() {
   const viewSwitch = document.getElementById("view-switch");
   const iconSwitch = document.getElementById("switchButton");
@@ -352,27 +34,39 @@ function handleSwitchView() {
   }, 2000)
 }
 
+
+
+
 /**
- *  function displays alert with title of <li>-element
- *
- *  @param li element to show title from
+ * JSL 1 
+ * Function to do an alert by clicking on a li element in both views
+ *  alerts the title of the list element
  */
-function showTitle(li){
-  document.addEventListener()
-  alert(li.querySelector(".attribute-title").innerHTML);
+
+var liItem = document.getElementsByClassName("list-items");
+for(var i = 0; i < liItem.length; i++){
+  liItem[i].addEventListener("click", function() { 
+    alert("Title: " + this.getElementsByClassName("attribute-title")[0].innerHTML);
+    });
 }
 
-function handleAlert() {
-const button = document.getElementsByClassName('myButton');
 
-button.addEventListener('click', function() {
-  // code to run when button is clicked
-  alert('Your message here');
-
-});
-
+/**
+ * JSL 1 
+ * Function to do an alert by clicking on the button of a li element in both views
+ *  alerts the title and URL of the list element
+ */
+const liButton = document.getElementsByClassName("list-element-menu");
+for(let i = 0; i < liButton.length; i++){
+  liButton[i].addEventListener("click", function(event) {
+    //hack to stop first alert function 
+    event.stopPropagation();  
+    alert("Title: " + document.getElementsByClassName("attribute-title")[i].innerHTML
+            +"\nURL: " + document.getElementsByClassName("attribute-origin")[i].innerHTML);
+  });
 }
 
-const alert =document.getElementsByClassName(".attribute-title");
-  alert(alert.innertext("attribute-title"));
+
+
+
 
